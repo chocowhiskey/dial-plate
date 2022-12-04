8ac9c933b0b60fcdce6d3e5a518a55fb1a45fe86
Simone Hoffmann <chocowhiskey_mf@gmx.de>
2022-12-04 16:11:24 +0100

    Todo: Substitute div by svg

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function drawCircle() {
+function drawCircle(
+  backgroundColor = "palegreen",
+  backgroundColorSmallCircles = "transparent"
+) {
   // test comment
+  // div circle mit svg ersetzen
   return (
     <div className="circle-parent">
       <div
         className="circle"
-        onClick={() => console.log("I was clicked!")}
-      ></div>
+        style={{ backgroundColor: backgroundColor }}
+        onMouseDown={mouseDown}
+      >
+        <div
+          className="smallCircle"
+          style={{ backgroundColor: backgroundColorSmallCircles }}
+          onClick={() => console.log("I was clicked!")}
+        ></div>
+      </div>
     </div>
   );
+}
+
+function mouseDown() {
+  console.log("i am mouse down");
+  console.log(document.getElementsByClassName("circle"));
+  // document.getElementsByClassName("circle").style.color = "red";
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {drawCircle()}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {drawCircle()}
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
